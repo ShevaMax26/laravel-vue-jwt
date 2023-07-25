4023d5600c437d871be4538f7b05e27b461a486d
ShevaMax26 <shevchuk.maxym@gmail.com>
2023-07-25 18:10:38 +0300

    Install JWT-token. Test queries in PostMan

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login',  [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [App\Http\Controllers\AuthController::class, 'me']);
+});
+
 Route::group(['prefix' => 'users'], function () {
     Route::post('/', User\StoreController::class);
 });
